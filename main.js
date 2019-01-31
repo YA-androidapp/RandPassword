bf08ea46ff83a1537408558ba5a37ddae4109cd8
YA <YA-androidapp@users.noreply.github.com>
2019-02-01 08:43:39 +0900

    done

--- main.js
+++ main.js
@@ -1,31 +1,40 @@
-var string = "";
+document.getElementById("button-gen").onclick = function() {
+    var string = acquireString();
+    document.getElementById("result").value = gen(string);
+};
 
-var checks = document.getElementsByClassName("form-check-input");
-for (let index = 0; index < checks.length; index++) {
-    const check = checks[index];
-    var check_id = check.getAttribute("id");
-    var text_ele = document.getElementById(check_id.replace("check-","text-"));
-    var text_id = text_ele.getAttribute("id");
+function acquireString(){
+    var string = "";
+    var checks = document.getElementsByClassName("form-check-input");
+    for (let index = 0; index < checks.length; index++) {
+        const check = checks[index];
+        var check_id = check.getAttribute("id");
+        var text_ele = document.getElementById(check_id.replace("check-","text-"));
+        var text_id = text_ele.getAttribute("id");
 
-    if(check.getAttribute("checked") == "checked"){
-        string += text_ele.value;
+        if(check.checked){
+            string += text_ele.value;
+        }
     }
+
+    return string;
 }
 
-console.log(string);
-
-
-
-
-
-
-
 function gen(string){
-    // string = 'abcdefghijklmnopqrstuvwxyz';
-    var length = Number(document.getElementById("text-length"));
+    var length = Number(document.getElementById("text-length").value);
     var result = '';
     for (var i = 0; i < length; i++) {
         result += string.charAt(Math.floor(Math.random() * string.length));
     }
     return result;
+}
+
+document.getElementById("button-copy").onclick = function() {
+    copy();
+};
+
+function copy() {
+    var result = document.getElementById("result");
+    result.select();
+    document.execCommand("Copy");
 }
